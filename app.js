821437d30183c0ccb5043b2fe6cb31ffc4dcb357
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-09-15 00:48:43 +0530

    after implementing clicking feature with color

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls')
 const sectBtn = document.querySelectorAll('.control')
-const allSections = document.querySelectorAll('.main-content')
+const allSections = document.querySelector('.main-content')
 
 
-function pageTransitions() {
+function PageTransitions() {
     for(let i = 0; i <= sectBtn.length-1; i++ ){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
@@ -12,6 +12,23 @@ function pageTransitions() {
             this.className = this.className + ' active-btn'
         })
     }
+
+    allSections.addEventListener('click', (e) => {  
+        const id = e.target.dataset.id
+
+        if (id){
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id)
+            element.classList.add('active')
+            }
+    })
 }
 
-pageTransitions()
+PageTransitions()
